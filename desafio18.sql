d411d9a5cfd594b31db9ec8c5967b29e80e7a403
Diego Siqueira <diego.camomila@gmail.com>
2022-05-30 16:56:57 -0300

    correçoes desafio 3, 18, 26

--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,2 @@
-SELECT COUNT(*) AS orders_count FROM northwind.orders 
-    WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
+SELECT purchase_orders FROM northwind.orders 
+    WHERE  suplier_id = 3 AND status_id = 2;
